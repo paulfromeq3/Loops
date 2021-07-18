4f45d87ab920b7be398c80df17fcfd7d6bd2427c
paulfromeq3 <paul.walker@eq3.org>
2021-07-18 18:10:30 -0500

    Logic changes in while / do while comparison

--- 02.MiniWhileGame/EntryPoint.cs
+++ 02.MiniWhileGame/EntryPoint.cs
@@ -74,71 +74,95 @@ class EntryPoint
         //Console.WriteLine($"Congratulations! {number} is a valid number!");
 
 
-        // Challenge #3
-        // Mini while game
-        string mageName = "Mage Henry";
-        string warriorName = "Warrior Sally";
+        //// Challenge #3
+        //// Mini while game
+        //string mageName = "Mage Henry";
+        //string warriorName = "Warrior Sally";
 
 
-        int mageHealth = 99;
-        int warriorHealth = 99;
-        int damage = 0;
+        //int mageHealth = 99;
+        //int warriorHealth = 99;
+        //int damage = 0;
 
-        // Random is the type.  It creates a random number generator.
-        // I used 'hit' but a common name is rng for "random number generator."
-        Random hit = new Random();
+        //// Random is the type.  It creates a random number generator.
+        //// I used 'hit' but a common name is rng for "random number generator."
+        //Random hit = new Random();
 
-        bool gameOver = false;
-        string message2 = " Character Health ";
+        //bool gameOver = false;
+        //string message2 = " Character Health ";
 
-        string separator2 = new string('=', message2.Length);
-        
-        while (!gameOver)  // Keep the logic of the "game over" concept as true or false, just use the ! "not" operator to express it for the loop
+        //string separator2 = new string('=', message2.Length);
+
+        //while (!gameOver)  // Keep the logic of the "game over" concept as true or false, just use the ! "not" operator to express it for the loop
+        //{
+        //    Console.ResetColor();
+        //    Console.Clear();
+
+        //    // Mage strikes first
+        //    Console.ForegroundColor = ConsoleColor.Blue;
+        //    damage = hit.Next(0, 15);
+        //    warriorHealth -= damage;
+        //    if (warriorHealth <= 0)
+        //    {
+        //        warriorHealth = 0;
+        //        Console.ForegroundColor = ConsoleColor.Green;
+        //        Console.WriteLine($"{warriorName} has been struck down!  {mageName} is victorious!!");
+        //        gameOver = true;  // Even though this is true, the condition on the while loop is based on the opposite "while game is not over do this..."
+        //    }
+        //    else if (mageHealth > 0)
+        //    {
+        //    Console.WriteLine($"{mageName} attacks {warriorName} for {damage}");
+        //    }
+
+        //    // Warrior strikes back
+        //    Console.ForegroundColor = ConsoleColor.Cyan;
+
+        //    damage = hit.Next(0, 15);
+        //    mageHealth -= damage;
+        //    if (mageHealth <= 0)
+        //    {
+        //        mageHealth = 0;
+        //        Console.ForegroundColor = ConsoleColor.Green;
+        //        Console.WriteLine($"{mageName} has been struck down!  {warriorName} is victorious!!");
+        //        gameOver = true;
+        //    }
+        //    else if (warriorHealth > 0)
+        //    {
+        //        Console.WriteLine($"{warriorName} attacks {mageName} for {damage}");
+        //    }
+
+        //    Console.ResetColor();
+        //    Console.WriteLine($"\n{message2}{message2,20}");
+        //    Console.WriteLine($"{separator2}{separator2,20}");
+        //    Console.WriteLine($" {mageHealth} <-{mageName}{warriorHealth,6} <-{warriorName}");
+        //    Thread.Sleep(2000);
+
+        //}
+        //Console.ResetColor();
+
+
+
+        // Comparison
+        // while loop
+        // the condition is checked first so the code never executes
+        int firstNumber = 11;
+        int endNumber = 10;
+        while (firstNumber <= endNumber)
         {
-            Console.ResetColor();
-            Console.Clear();
-
-            // Mage strikes first
-            Console.ForegroundColor = ConsoleColor.Blue;
-            damage = hit.Next(0, 15);
-            warriorHealth -= damage;
-            if (warriorHealth <= 0)
-            {
-                warriorHealth = 0;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{warriorName} has been struck down!  {mageName} is victorious!!");
-                gameOver = true;  // Even though this is true, the condition on the while loop is based on the opposite "while game is not over do this..."
-            }
-            else if (mageHealth > 0)
-            {
-            Console.WriteLine($"{mageName} attacks {warriorName} for {damage}");
-            }
-
-            // Warrior strikes back
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
-            damage = hit.Next(0, 15);
-            mageHealth -= damage;
-            if (mageHealth <= 0)
-            {
-                mageHealth = 0;
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{mageName} has been struck down!  {warriorName} is victorious!!");
-                gameOver = true;
-            }
-            else if (warriorHealth > 0)
-            {
-                Console.WriteLine($"{warriorName} attacks {mageName} for {damage}");
-            }
-
-            Console.ResetColor();
-            Console.WriteLine($"\n{message2}{message2,20}");
-            Console.WriteLine($"{separator2}{separator2,20}");
-            Console.WriteLine($" {mageHealth} <-{mageName}{warriorHealth,6} <-{warriorName}");
-            Thread.Sleep(2000);
-
+            Console.WriteLine(firstNumber);
+            firstNumber++;
         }
-        Console.ResetColor();
+        Console.WriteLine();
+
+        // do while loop
+        // the condition is checked after the code runs, so it always runs at least once
+        firstNumber = 11;
+        endNumber = 10;
+        do
+        {
+            Console.WriteLine(firstNumber);
+            firstNumber++;
+        } while (firstNumber <= endNumber);
 
 
     }
