ddd9187e63277d4d9717d8bee25d9b7f65603335
paulfromeq3 <paul.walker@eq3.org>
2021-07-19 12:45:08 -0500

    Nested loop examples, multiple ways of doing things

--- 03.ForLoops/EntryPoint.cs
+++ 03.ForLoops/EntryPoint.cs
@@ -53,190 +53,221 @@ class EntryPoint
         //    Console.WriteLine();
         //}
 
+        /////////////////////////////////////////////////
+        //// More "for" loop practice
+        //// Interacting with an array
 
-        // More "for" loop practice
-        string menu = "1. Add new Item" +
-            "\n2. Edit Item" +
-            "\n3. Remove Item" +
-            "\n4. View all Items" +
-            "\n5. Exit\n";
+        //string menu = "1. Add new Item" +
+        //    "\n2. Edit Item" +
+        //    "\n3. Remove Item" +
+        //    "\n4. View all Items" +
+        //    "\n5. Exit\n";
 
-        string[] items = new string[10];  // This is the array that we'll work with.  It will have 10 items.
-        items[0] = "apple";
-        items[1] = "orange";
-        //string[] tempArray = new string[10];  // Assign this in Case 3. This array is used to resort the items[] array to take out the null values
+        //string[] items = new string[10];  // This is the array that we'll work with.  It will have 10 items.
+        //items[0] = "apple";
+        //items[1] = "orange";
+        ////string[] tempArray = new string[10];  // Assign this in Case 3. This array is used to resort the items[] array to take out the null values
 
 
-        string fruit = null;
-        bool exit = false;
-        int selectedItem = 0;
+        //string fruit = null;
+        //bool exit = false;
+        //int selectedItem = 0;
 
-        Console.WriteLine(menu);
-        while (!exit)
+        //Console.WriteLine(menu);
+        //while (!exit)
+        //{
+        //    Console.Write("Your choice:  ");
+        //    int userChoice = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine();
+
+        //    // NOTE:  For each case below, I highlighted and hit control-r-m to extract a method from the code
+        //    // This is the first time I've created a method in C#, and first time using the shortcut
+        //    // So far, it seems to work ok
+        //    // I simply selected everything in between "case" and "break" 
+
+        //    switch (userChoice)
+        //    {
+        //        case 1:
+        //            fruit = AddAnItem(items, fruit);
+        //            break;
+
+        //        case 2:
+        //            selectedItem = EditAnItem(items, ref fruit);
+        //            break;
+
+        //        case 3:
+        //            selectedItem = RemoveItem(items);
+        //            break;
+
+        //        case 4:
+        //            ViewAllItems(items);
+        //            break;
+
+        //        case 5:
+        //            exit = WantToExit();
+        //            break;
+
+        //        default:
+        //            Console.WriteLine("Unrecognized option. Please choose a menu item between 1 and 5");
+        //            break;
+        //    }
+        //    Console.Clear();
+        //    Console.WriteLine(menu);
+
+        //}
+
+        /////////////////////////////
+        // The foreach loop
+        // - for loops are derivatives of the while loop
+        // - foreach loops are derivatives of the for loop
+        // - the foreach loop is specifically designed to work on collections of data = including arrays
+
+        // display the contents of this array with a for loop
+        int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+        for (int i = 0; i < array.Length; i++)
         {
-            Console.Write("Your choice:  ");
-            int userChoice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            // NOTE:  For each case below, I highlighted and hit control-r-m to extract a method from the code
-            // This is the first time I've created a method in C#, and first time using the shortcut
-            // So far, it seems to work ok
-            // I simply selected everything in between "case" and "break"
-
-            switch (userChoice)
-            {
-                case 1:
-                    fruit = AddAnItem(items, fruit);
-                    break;
-
-                case 2:
-                    selectedItem = EditAnItem(items, ref fruit);
-                    break;
-
-                case 3:
-                    selectedItem = RemoveItem(items);
-                    break;
-
-                case 4:
-                    ViewAllItems(items);
-                    break;
-
-                case 5:
-                    exit = WantToExit();
-                    break;
-
-                default:
-                    Console.WriteLine("Unrecognized option. Please choose a menu item between 1 and 5");
-                    break;
-            }
-            Console.Clear();
-            Console.WriteLine(menu);
-
+            Console.WriteLine(array[i]);
         }
 
-    }
-
-    private static string AddAnItem(string[] items, string fruit)
-    {
-        // Add an item
-        // First, iterate through the array to find the next available index
-        for (int i = 0; i < items.Length; i++)
-        {
-            if (items[i] == null)  // null means there's nothing there in that index position
-            {
-                Console.WriteLine("Enter the name of a fruit:  ");
-                fruit = Console.ReadLine();
-                items[i] = fruit;
-                break;
-            }
-        }
-
-        return fruit;
-    }
-
-    private static int EditAnItem(string[] items, ref string fruit)
-    {
-        int selectedItem;
-        // Edit an item
-        for (int i = 0; i < items.Length; i++)
-        {
-            if (items[i] != null)
-            {
-                Console.WriteLine($"{i + 1}.  {items[i]}");
-
-            }
-        }
+        // now display the same contents with a foreach loop
+        // foreach works a little different than the for loop
+        // With a for loop, you can manipulate the index and skip items.  Index is used like a counter. And you don't have to use it on collections. 
+        // With a foreach loop, you can't do that.  You go through each item in the array. You can only use it on collections.
+        // "foreach" element of the collection, "foreach" element of the array...
         Console.WriteLine();
-        Console.Write("Which item number do you want to edit? # ");
-        selectedItem = Convert.ToInt32(Console.ReadLine());
-
-        if ((selectedItem - 1 >= 0) && (selectedItem < items.Length) && (items[selectedItem - 1] != null))
+        foreach (var item in array)
         {
-            Console.Write("Please enter a new fruit to substitute for ");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"{items[selectedItem - 1]}");
-            Console.ResetColor();
-            Console.Write(" : ");
-            fruit = Console.ReadLine();
-            items[selectedItem - 1] = fruit;
-        }
-        else
-        {
-            Console.WriteLine("Selection is invalid");
+            Console.WriteLine(item);
         }
 
-        Console.Write("\nPress enter to continue: ");
-        Console.ReadLine();
-        return selectedItem;
+
+
+
     }
 
-    private static bool WantToExit()
-    {
-        bool exit;
-        // Exit
-        Console.WriteLine("You chose to exit");
-        exit = true;
-        return exit;
-    }
+    ////////////////////////////////////////////////////////////////////
+    // The following are methods created for "Interacting with an array" loop practice
+    //private static string AddAnItem(string[] items, string fruit)
+    //{
+    //    // Add an item
+    //    // First, iterate through the array to find the next available index
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        if (items[i] == null)  // null means there's nothing there in that index position
+    //        {
+    //            Console.WriteLine("Enter the name of a fruit:  ");
+    //            fruit = Console.ReadLine();
+    //            items[i] = fruit;
+    //            break;
+    //        }
+    //    }
 
-    private static void ViewAllItems(string[] items)
-    {
-        // View all current items
-        // But don't show the null items of the array
-        for (int i = 0; i < items.Length; i++)
-        {
-            if (items[i] != null)
-            {
-                Console.WriteLine($"{i + 1}. {items[i]}");
-            }
-        }
-        Console.Write("\nPress enter to continue");
-        Console.ReadLine();
-    }
+    //    return fruit;
+    //}
 
-    private static int RemoveItem(string[] items)
-    {
-        // Remove an item
-        int selectedItem = 0;
-        for (int i = 0; i < items.Length; i++)
-        {
-            if (items[i] != null)
-            {
-                Console.WriteLine($"{i + 1}. {items[i]}");
-            }
-        }
+    //private static int EditAnItem(string[] items, ref string fruit)
+    //{
+    //    int selectedItem;
+    //    // Edit an item
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        if (items[i] != null)
+    //        {
+    //            Console.WriteLine($"{i + 1}.  {items[i]}");
 
-        Console.Write("Enter the index number to remove an item: ");
-        selectedItem = Convert.ToInt32(Console.ReadLine());
-        if ((selectedItem - 1 >= 0) && (selectedItem <= items.Length) && (items[selectedItem - 1] != null))
-        {
-            items[selectedItem - 1] = null;
-            string[] tempArray = new string[10];
-            int simpleCounter = 0;
-            for (int j = 0; j < items.Length; j++)
-            {
-                if (items[j] != null)
-                {
-                    tempArray[simpleCounter] = items[j];
-                    simpleCounter++;
-                }
-            }
-            //// This is one method to copy the temporary array back to the main array
-            //for (int j = 0; j < tempArray.Length; j++)
-            //{
-            //    items[j] = tempArray[j];
-            //}
+    //        }
+    //    }
+    //    Console.WriteLine();
+    //    Console.Write("Which item number do you want to edit? # ");
+    //    selectedItem = Convert.ToInt32(Console.ReadLine());
 
-            // This is another method to copy one array to another
-            Array.Copy(tempArray, items, tempArray.Length);
+    //    if ((selectedItem - 1 >= 0) && (selectedItem < items.Length) && (items[selectedItem - 1] != null))
+    //    {
+    //        Console.Write("Please enter a new fruit to substitute for ");
+    //        Console.ForegroundColor = ConsoleColor.Red;
+    //        Console.Write($"{items[selectedItem - 1]}");
+    //        Console.ResetColor();
+    //        Console.Write(" : ");
+    //        fruit = Console.ReadLine();
+    //        items[selectedItem - 1] = fruit;
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Selection is invalid");
+    //    }
 
-        }
-        else
-        {
-            Console.WriteLine("Selection is invalid");
-        }
-        Console.Write("\nPress enter to continue");
-        Console.ReadLine();
-        return selectedItem;
-    }
+    //    Console.Write("\nPress enter to continue: ");
+    //    Console.ReadLine();
+    //    return selectedItem;
+    //}
+
+    //private static bool WantToExit()
+    //{
+    //    bool exit;
+    //    // Exit
+    //    Console.WriteLine("You chose to exit");
+    //    exit = true;
+    //    return exit;
+    //}
+
+    //private static void ViewAllItems(string[] items)
+    //{
+    //    // View all current items
+    //    // But don't show the null items of the array
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        if (items[i] != null)
+    //        {
+    //            Console.WriteLine($"{i + 1}. {items[i]}");
+    //        }
+    //    }
+    //    Console.Write("\nPress enter to continue");
+    //    Console.ReadLine();
+    //}
+
+    //private static int RemoveItem(string[] items)
+    //{
+    //    // Remove an item
+    //    int selectedItem = 0;
+    //    for (int i = 0; i < items.Length; i++)
+    //    {
+    //        if (items[i] != null)
+    //        {
+    //            Console.WriteLine($"{i + 1}. {items[i]}");
+    //        }
+    //    }
+
+    //    Console.Write("Enter the index number to remove an item: ");
+    //    selectedItem = Convert.ToInt32(Console.ReadLine());
+    //    if ((selectedItem - 1 >= 0) && (selectedItem <= items.Length) && (items[selectedItem - 1] != null))
+    //    {
+    //        items[selectedItem - 1] = null;
+    //        string[] tempArray = new string[10];
+    //        int simpleCounter = 0;
+    //        for (int j = 0; j < items.Length; j++)
+    //        {
+    //            if (items[j] != null)
+    //            {
+    //                tempArray[simpleCounter] = items[j];
+    //                simpleCounter++;
+    //            }
+    //        }
+    //        //// This is one method to copy the temporary array back to the main array
+    //        //for (int j = 0; j < tempArray.Length; j++)
+    //        //{
+    //        //    items[j] = tempArray[j];
+    //        //}
+
+    //        // This is another method to copy one array to another
+    //        Array.Copy(tempArray, items, tempArray.Length);
+
+    //    }
+    //    else
+    //    {
+    //        Console.WriteLine("Selection is invalid");
+    //    }
+    //    Console.Write("\nPress enter to continue");
+    //    Console.ReadLine();
+    //    return selectedItem;
+    //}
 }
---
+++ 04.NestedLoops/EntryPoint.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        //// Nested loops
+        //// used for more complicated operations or for multi-diminsional arrays
+        //// This is how a multi-diminsional array index might look
+        //// Or maybe a table of data, like date and temperature for each day
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    for (int j = 0; j < 10; j++)
+        //    {
+        //        Console.Write($"index[{i}][{j}] ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+
+        // Nested loop Challenge #1
+        // Display the month, the day of the month and the day of the week
+
+        string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        string[] days = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+        int daysinMonth = 31;
+        int daysOfWeek = 0;
+
+        for (int currentMonth = 0; currentMonth < months.Length; currentMonth++)
+        {
+            Console.WriteLine($"Month: {months[currentMonth]} ");
+            for (int currentDay = 1; currentDay <= daysinMonth; currentDay++)
+            {
+                Console.WriteLine($"   {currentDay} {days[daysOfWeek]}");
+
+                daysOfWeek++;
+                if (daysOfWeek == 7)
+                {
+                    daysOfWeek = 0;
+                }
+                if (currentMonth == 1 && currentDay == 28)
+                {
+                    break;
+                }
+                if ((currentMonth == 3 || currentMonth == 5 || currentMonth == 8 || currentMonth == 10 ) && currentDay == 30)
+                {
+                    break;
+                }
+
+            }
+
+
+            // Below is the first way I did it
+            // Use longMonthDates for months with 31 days
+            //int dayCounter = 0;
+            //if (item == "January" || item == "March" || item == "May" || item == "July" || item == "August" || item == "October" || item == "December")
+            //{
+            //    for (int i = 0; i < 31; i++)
+            //    {
+            //        Console.WriteLine($"{i+1} {week[dayCounter]}");
+            //        dayCounter++;
+            //        if (dayCounter >= 7)
+            //        {
+            //            dayCounter = 0;
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+            //// Use febMonthDates if the month is February
+            //else if (item == "February")
+            //{
+            //    for (int j = 0; j < 28; j++)
+            //    {
+            //        Console.WriteLine($"{j+1} {week[dayCounter]}");
+            //        dayCounter++;
+            //        if (dayCounter >= 7)
+            //        {
+            //            dayCounter = 0;
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+            //// Use shortMonthDates for everything else
+            //else
+            //{
+            //    for (int k = 0; k < 30; k++)
+            //    {
+            //        Console.WriteLine($"{k+1} {week[dayCounter]}");
+            //        dayCounter++;
+            //        if (dayCounter >= 7)
+            //        {
+            //            dayCounter = 0;
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
